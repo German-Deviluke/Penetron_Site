903c7a256f1131755eddb8ceb0a6f55f70531836
German Deviluke <momo.san@list.ru>
2025-05-21 15:48:01 +0500

    zxc

--- js/script.js
+++ js/script.js
@@ -1,25 +1,24 @@
+const slogans = [
+  "Не ссы пронесёт,<br>Защита бетона целый год.",
+  "Природная мощь. Технологичная защита.",
+  "Там, где стихий — там Пенетрон.",
+  "Сила гор. Защита от воды. Надёжность навсегда.",
+  "Пенетрон. Стойкость, проверенная природой."
+];
 
-  const slogans = [
-    "Не ссы пронесет,<br>Защита бетона целый год.",
-    "Природная мощь. Технологичная защита.",
-    "Там, где стихия — там Пенетрон.",
-    "Сила гор. Защита от воды. Надёжность навсегда.",
-    "Пенетрон. Стойкость, проверенная природой."
-  ];
+let index = 0;
+const sloganElement = document.getElementById("slogan");
 
-  let index = 0;
-  const sloganElement = document.getElementById("slogan");
+function updateSlogan() {
+  sloganElement.classList.remove("fade-in");
+  sloganElement.style.opacity = 0;
 
-  function updateSlogan() {
-    sloganElement.classList.remove("fade-in");
-    sloganElement.style.opacity = 0;
+  setTimeout(() => {
+    index = (index + 1) % slogans.length;
+    sloganElement.innerHTML = slogans[index];
+    sloganElement.classList.add("fade-in");
+    sloganElement.style.opacity = 1;
+  }, 300);
+}
 
-    setTimeout(() => {
-      index = (index + 1) % slogans.length;
-      sloganElement.innerHTML = slogans[index];
-      sloganElement.classList.add("fade-in");
-      sloganElement.style.opacity = 1;
-    }, 300);
-  }
-
-  setInterval(updateSlogan, 5000);
+setInterval(updateSlogan, 5000);
