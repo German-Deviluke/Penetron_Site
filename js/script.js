57b49b6014a381b9ac416ad38aba2890b6be18fa
German Deviluke <momo.san@list.ru>
2025-05-21 17:50:00 +0500

    zxc

--- js/script.js
+++ js/script.js
@@ -11,13 +11,13 @@ const sloganElement = document.getElementById("slogan");
 
 function updateSlogan() {
   sloganElement.classList.remove("fade-in");
-  sloganElement.style.opacity = 0;
+  sloganElement.style.opacity = 1;
 
   setTimeout(() => {
     index = (index + 1) % slogans.length;
     sloganElement.innerHTML = slogans[index];
     sloganElement.classList.add("fade-in");
-    sloganElement.style.opacity = 1;
+    sloganElement.style.opacity = 2;
   }, 300);
 }
 
